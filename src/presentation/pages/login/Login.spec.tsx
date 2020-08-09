eacb8128f8244b226c2ee2a2fe84ac8975527b2f
georgekaran <georgekaran12@gmail.com>
2020-08-09 20:26:24 -0300

    refactor: move AuthenticationSpy to its own file

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -3,19 +3,7 @@ import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react
 import faker from 'faker'
 
 import Login from './Login'
-import { ValidationStub } from '@/presentation/test'
-import { Authentication, AuthenticationParams } from '@/domain/usecases'
-import { AccountModel } from '@/domain/models'
-import { mockAccountModel } from '@/domain/test'
-
-class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
-  params: AuthenticationParams
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    this.params = params
-    return Promise.resolve(this.account)
-  }
-}
+import { ValidationStub, AuthenticationSpy } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
