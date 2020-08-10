8796656199a9d5d7568f58f21b53deb1edcfe9cf
georgekaran <georgekaran12@gmail.com>
2020-08-10 09:59:25 -0300

    feat: ensure Login will not call Authentication more than once

--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -38,6 +38,9 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) {
+      return
+    }
     setState({
       ...state,
       isLoading: true
