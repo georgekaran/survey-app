96fc9f1c58fd13bb02ca73b82ee5ca72fd50b95d
georgekaran <georgekaran12@gmail.com>
2020-08-14 16:01:31 -0300

    test: ensure Signup show valid email state if Validation succeeds

--- src/presentation/pages/signup/Signup.spec.tsx
+++ src/presentation/pages/signup/Signup.spec.tsx
@@ -73,4 +73,10 @@ describe('Signup Page', () => {
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name')
   })
+
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
 })
