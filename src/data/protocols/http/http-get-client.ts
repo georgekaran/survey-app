596bef1679ab0f5460fa776a840c886ef6acff18
georgekaran <georgekaran12@gmail.com>
2020-08-18 14:56:11 -0300

    feat: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -1,5 +1,7 @@
-export interface HttpGetClient {
-  get: (params: HttpGetParams) => Promise<void>
+import { HttpResponse } from './http-response'
+
+export interface HttpGetClient<R = any> {
+  get: (params: HttpGetParams) => Promise<HttpResponse<R>>
 }
 
 export type HttpGetParams = {
