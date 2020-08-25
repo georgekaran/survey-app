129b4d6b8d684ad37be985b9a73aacd4911525d0
georgekaran <georgekaran12@gmail.com>
2020-08-25 10:40:37 -0300

    refactor: change validation factory to not use builder

--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -1,9 +1,10 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 
 export const makeLoginValidation = (): ValidationComposite => {
   return ValidationComposite.build([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
+    new RequiredFieldValidation('email'),
+    new EmailValidation('email'),
+    new RequiredFieldValidation('password'),
+    new MinLengthValidation('password', 5)
   ])
 }
--- src/main/factories/pages/signup/signup-validation-factory.ts
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -1,11 +1,14 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite, RequiredFieldValidation, MinLengthValidation, EmailValidation, CompareFieldsValidation } from '@/validation/validators'
 
 export const makeSignupValidation = (): ValidationComposite => {
   return ValidationComposite.build([
-    ...ValidationBuilder.field('name').required().min(5).build(),
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build(),
-    ...ValidationBuilder.field('passwordConfirmation').required().sameAs('password').build()
+    new RequiredFieldValidation('name'),
+    new MinLengthValidation('name', 5),
+    new RequiredFieldValidation('email'),
+    new EmailValidation('email'),
+    new RequiredFieldValidation('password'),
+    new MinLengthValidation('password', 5),
+    new RequiredFieldValidation('passwordConfirmation'),
+    new CompareFieldsValidation('passwordConfirmation', 'password')
   ])
 }
