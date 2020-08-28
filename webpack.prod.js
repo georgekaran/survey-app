5ec32d375e872d22d4c46dbddf7ecd1be994a553
georgekaran <georgekaran12@gmail.com>
2020-08-27 23:01:20 -0300

    feat: add favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { DefinePlugin } = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FaviconWebpackPlugin = require('favicons-webpack-plugin')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
@@ -42,6 +43,9 @@ module.exports = merge(common, {
     }),
     new MiniCssExtractPlugin({
       filename: 'main-bundle-[hash].css'
+    }),
+    new FaviconWebpackPlugin({
+      logo: './public/favicon.png'
     })
   ]
 })
