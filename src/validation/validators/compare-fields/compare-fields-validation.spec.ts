e9623f8292dfca9677171dcaa296e768ccfe6050
georgekaran <georgekaran12@gmail.com>
2020-08-27 22:16:39 -0300

    fix: ensure CompareFieldsValidation dont generate same fields name to compare

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -6,19 +6,19 @@ const makeSut = (fieldName: string, fieldToCompare: string): CompareFieldsValida
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const fieldName = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const fieldName = 'any_field'
+    const fieldToCompare = 'other_field'
     const sut = makeSut(fieldName, fieldToCompare)
     const error = sut.validate({
-      [fieldToCompare]: 'any_value',
-      [fieldName]: 'other_value'
+      [fieldName]: 'any_value',
+      [fieldToCompare]: 'other_value'
     })
     expect(error).toEqual(new InvalidFieldError(fieldName))
   })
 
   test('Should return falsy if compare is valid', () => {
-    const fieldName = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const fieldName = 'any_field'
+    const fieldToCompare = 'other_field'
     const value = faker.random.word()
     const sut = makeSut(fieldName, fieldToCompare)
     const error = sut.validate({
